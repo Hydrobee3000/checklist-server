54d4f764c4807eba4d55455566d2582a5932067b
Алексей Заикин <hydrombee@gmail.com>
2024-02-18 12:07:57 +0100

    added mongoDB connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const PORT = 5000
 
@@ -8,6 +9,8 @@ app.use(express.json()) // to convert json format
 
 async function startApp() {
   try {
+    await mongoose.connect(DB_URL) // connection to mongoDB
+
     // listen for changes on the specified port
     app.listen(PORT, () => {
       console.log(`Server Started at ${PORT}`)
